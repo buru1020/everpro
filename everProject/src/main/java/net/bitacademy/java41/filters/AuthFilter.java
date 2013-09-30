1876278e9676c123ee1037f36894a6a07bec2e6f
Buru <Buru@Haapy>
2013-09-30 19:43:19 +0900

    김상헌		2013-09-30	로그인 호출 url 변경

--- everProject/src/main/java/net/bitacademy/java41/filters/AuthFilter.java
+++ everProject/src/main/java/net/bitacademy/java41/filters/AuthFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -40,7 +39,7 @@ public class AuthFilter implements Filter {
 		} else {
 			response.sendRedirect(
 					request.getServletContext().getContextPath() + 
-					"/auth/loginForm.do");
+					"/auth/login.do");
 		}
 	}
 
